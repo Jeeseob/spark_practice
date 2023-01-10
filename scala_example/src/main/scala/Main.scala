a738578b962a85f4ebe52c9fcaac1e5fcd26f953
= <=>
2023-01-10 13:45:32 +0900

    create resources dir and wordCount file

--- scala_example/src/main/scala/Main.scala
+++ scala_example/src/main/scala/Main.scala
@@ -7,22 +7,22 @@ object Main {
     val conf = new SparkConf().setMaster("local").setAppName("wordCount")
     val sc = new SparkContext(conf)
 
-    val textRDD = sc.textFile("README.md")
+    val textRDD = sc.textFile("./resources/wordCount.txt")
     val counts = textRDD.flatMap(line => line.split(" "))
       .map(word => (word, 1))
       //shuffle
       .reduceByKey(_ + _)
 
-    val countsCache = counts.cache()
-    countsCache.collect()
+    val countsCache = counts.cache()  // caching
     countsCache.foreach(tuple => println(tuple._1, tuple._2))
 
     println("--------------------------------------------------")
 
     val sortedRDD = countsCache.map(tuple => (tuple._2, tuple._1))
       //shuffle
-      .sortByKey(false)
+      .sortByKey(ascending = false)
       .map(tuple => (tuple._2, tuple._1))
-      .foreach(tuple => println(tuple._1, tuple._2))
+
+    sortedRDD.foreach(tuple => println(tuple._1, tuple._2))
   }
 }
